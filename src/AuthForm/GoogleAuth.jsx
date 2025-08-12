6926dfd9e621a9556fd2e18171b1c96d66b77b56
Sainath Mhetre <70021060+lordsainath@users.noreply.github.com>
2025-08-13 00:24:13 +0530

    refactor(ui): changes in AuthForm on GoogleLogin (#1)
    * login implemented with firebase
    
    * refactor(ui): changes in AuthForm on GoogleLogin

--- src/AuthForm/GoogleAuth.jsx
+++ src/AuthForm/GoogleAuth.jsx
@@ -1,14 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 
-const GoogleAuth = () => {
+const GoogleAuth = ({prefix}) => {
   return (
     <>
       <Box marginTop={4}>
         <div className="flex items-center justify-center gap-2">
           <img src="/google.png" className="w-[20px]" alt="" />
           <button className="!text-blue-700 cursor-pointer">
-            Log in with google
+            {prefix} with google
           </button>
         </div>
       </Box>
---
+++ src/hooks/useLoginWithEmailAndPassoword.jsx
@@ -0,0 +1,47 @@
+import { auth, database } from "@/firebase/firebaseConfig";
+import useAuthStore from "@/store/authStore";
+import { doc, getDoc } from "firebase/firestore";
+
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
+const useLoginWithEmailAndPassoword = () => {
+  const [signInWithEmailAndPassword, user, loginloading, loginerror] =
+    useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+  const loginUser = useAuthStore((state) => state.login);
+
+  const handleLogin = async (email, password) => {
+    if (!email || !password) {
+      console.log("Enter all fields");
+      return;
+    }
+
+    try {
+      const userlogin = await signInWithEmailAndPassword(email, password);
+      if (!userlogin && loginerror) {
+        console.log("error while login");
+        return;
+      }
+
+      const userDocRef = doc(database, "users", userlogin.user.uid);
+      const docSnap = await getDoc(userDocRef);
+
+      if (docSnap.exists()) {
+        console.log("User data:", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+      localStorage.setItem("user-info", JSON.stringify(docSnap.data()));
+
+      loginUser(docSnap.data());
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return [handleLogin, user, loginloading, loginerror];
+};
+
+export default useLoginWithEmailAndPassoword;
